672fc773a56296503fc968068fd0fa47c6de6d67
Jia Chengkun <ck@zhihu.com>
2015-06-26 15:45:10 +0800

    fix head repo

--- zhihu-buildout.rb
+++ zhihu-buildout.rb
@@ -5,7 +5,7 @@ class ZhihuBuildout < Formula
     url "https://github.com/lfyzjck/buildout/archive/2.3.1.tar.gz", :tag => "2.3.1"
     sha256 "20eaaa1d2dd08fd3b5536a05b202c3adb307c5792eec2fead6b106a39f30f65e"
     version "2.3.1"
-    head "/Users/jiachengkun/zhihu/buildout", :using => :git
+    head "https://github.com/lfyzjck/buildout.git", :using => :git
 
     def install
         inreplace "bin/buildout", /eggs/, "#{prefix}"
